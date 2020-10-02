5d38f4cb821596813174d4505adc7e9170d54202
Lois Simonca <loisgabor@gmail.com>
2020-10-02 18:20:31 -0400

    Created current date on top of the page

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
   console.log("this is working!");
+
+  // DOM Variable
+
+  var hour = $(".hour");
+  var timeBlock = $(".time-block");
+  var saveBtn = $(".saveBtn");
+  var currentDay = $("#currentDay");
+  // var textArea = $(".textarea");
+  var past = $(".past");
+  var present = $(".present");
+  var future = $(".future");
+  // JS Variable
+  var time = moment().format("dddd, MMMM Do, YYYY");
+
+  // $(".hours") = moment();
+  // console.log(hours);
+  // Funtion Definitions
+  $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+  // Function Calls
+
+  // Event Listener
 });
